ebfe6c4cafeeb5d05eb5fa43a2b81159405f4bd7
Sebastian Iversen <kierstejn1994@gmail.com>
2020-07-01 01:24:48 +0200

    added support for prod

--- cliffdivingsites.web/src/config.ts
+++ cliffdivingsites.web/src/config.ts
@@ -12,7 +12,7 @@ const prod = {
     oidc: {
         clientId: '0oahhxwioSbJAzJSm4x6',
         issuer: 'https://dev-880092.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:3000/implicit/callback',
+        redirectUri: 'https://keen-wiles-2fb95e.netlify.app/implicit/callback',
         scopes: ['openid', 'profile', 'email'],
         pkce: false
     }

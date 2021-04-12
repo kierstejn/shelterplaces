4c2b3d0c0ef12b54f1a517c7837a4f0ef17ce585
Sebastian Iversen <31436698+kierstejn@users.noreply.github.com>
2021-04-12 23:38:44 +0200

    Update config.ts

--- cliffdivingsites.web/src/config.ts
+++ cliffdivingsites.web/src/config.ts
@@ -1,22 +1,3 @@
-const dev = {
-    serverUrl: 'http://localhost:5000',
-    locationIq: {
-        token: '42871ecde74384'
-    },
-    google: {
-        apiKey: 'AIzaSyCnJaUXeaK95zS4ivA7xTmlKaiuWA9Mf_k'
-    }
-};
-
-const prod = {
-    serverUrl: '',
-    locationIq: {
-        token: '42871ecde74384'
-    },
-    google: {
-        apiKey: 'AIzaSyCnJaUXeaK95zS4ivA7xTmlKaiuWA9Mf_k'
-    }
-};
 
 
 const config = process.env.NODE_ENV === 'development' ? dev : prod;
